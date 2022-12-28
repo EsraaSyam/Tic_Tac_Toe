00b888f875f9240c18786aaebbbdafb274602dbd
Esraa Syam <112758974+EsraaSyam@users.noreply.github.com>
2022-12-28 19:40:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let title  = document.querySelector('.title');
 let tern = 'x'
 let squares = [];
 let cnt = 0;
+let flag = false;
 function win(n1 , n2 , n3){
     title.innerHTML = `${squares[n1]} is the winner`;
     document.getElementById('item' + n1).style.backgroundColor = 'red';
@@ -16,36 +17,41 @@ function winner(){
     }
     if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != ''){
         win(1,2,3);
-
+        flag = true;
     }
     if(squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != ''){
         win(4,5,6);
+        flag = true;
     }
     if(squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != ''){
         win(7,8,9);
+        flag = true;
     }
     if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != ''){
         win(1,4,7);
+        flag = true;
     }
     if(squares[2] == squares[5] && squares[5] == squares[8] && squares[2] != ''){
         win(2,5,8);
+        flag = true;
     }
     if(squares[3] == squares[6] && squares[6] == squares[9] && squares[3] != ''){
         win(3,6,9);
+        flag = true;
     }
     if(squares[1] == squares[5] && squares[5] == squares[9] && squares[1] != ''){
         win(1,5,9);
+        flag = true;
     }
     if(squares[3] == squares[5] && squares[5] == squares[7] && squares[3] != ''){
         win(3,5,7);
+        flag = true;
     }
-    if(cnt == 9){
+    if(cnt == 9 && flag == false){
         title.innerHTML = 'Draw';
         setInterval(function(){title.innerHTML += '.'}, 1000);
         setTimeout(function(){location.reload()}, 3000);
     }
-
-    
 
 }
 function game(id){
